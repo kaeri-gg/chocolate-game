c4c285576b4bdf8125c23f2d5f869ed24560a60a
Kathleen Povadora <60345682+kaeri-gg@users.noreply.github.com>
2026-03-03 23:31:18 +0200

    Add sticker assets and icon assignment
    Add multiple sticker PNGs and their Godot .import metadata (bueno, ferrero, kinder*, nutella, raffaello, image-x variants) and remove legacy stickers-01..08 assets. Update scene to use the new main_scene.tres theme resource and rename HBoxContainers to uniquely named grid containers (GridContainer1-4, GridContainer5-8, etc.), updating button parent paths. Update main_scene.gd: rename button_1, add grid container onready vars, preload icon and dummy textures, introduce buttons array, chocolate/streak counters, reset(), get_indexes(), and assign_icons() to randomly assign sticker icons to buttons. Also update theme/main_scene.tres UID to match the scene change.

--- script/main_scene.gd
+++ script/main_scene.gd
@@ -1,7 +1,7 @@
 class_name MainScene
 extends Control
 
-@onready var button: Button = %Button
+@onready var button_1: Button = %Button
 @onready var button_2: Button = %Button2
 @onready var button_3: Button = %Button3
 @onready var button_4: Button = %Button4
@@ -18,11 +18,70 @@ extends Control
 @onready var button_15: Button = %Button15
 @onready var button_16: Button = %Button16
 
+@onready var grid_container_1_4: HBoxContainer = %"GridContainer1-4"
+@onready var grid_container_5_8: HBoxContainer = %"GridContainer5-8"
+@onready var grid_container_9_12: HBoxContainer = %"GridContainer9-12"
+@onready var grid_container_13_16: HBoxContainer = %"GridContainer13-16"
+
+
+var buttons: Array[Button]
+var chocolate_count: int
+var streak_count: int
+
+var icon_textures = [
+	preload("res://assets/stickers/bueno.png"),
+	preload("res://assets/stickers/ferrero.png"),
+	preload("res://assets/stickers/kinder2.png"),
+	preload("res://assets/stickers/kinder3.png"),
+	preload("res://assets/stickers/kinder.png"),
+	preload("res://assets/stickers/nutella.png"),
+	preload("res://assets/stickers/raffaello.png")
+]
+
+var dummy_textures = [
+	preload("res://assets/stickers/image-x.png"),
+	preload("res://assets/stickers/image-x-1.png"),
+	preload("res://assets/stickers/image-x-2.png")
+]
+
+func reset() -> void:
+	chocolate_count = 4
+	streak_count = 0
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	reset()
+	buttons = [
+		button_1,
+		button_2,
+		button_3,
+		button_4,
+		button_5,
+		button_6,
+		button_7,
+		button_8,
+		button_9,
+		button_10,
+		button_11,
+		button_12,
+		button_13,
+		button_14,
+		button_15,
+		button_16
+	]
+		
+	assign_icons()
 
+func get_indexes() -> Dictionary[int, bool]:
+	var indexes: Dictionary[int, bool] = {}
+	
+	while indexes.size() < chocolate_count:
+		var random_index = randi_range(0, buttons.size() -1)
+		indexes.set(random_index, true)
+		
+	return indexes
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func assign_icons() -> void:
+	for index in get_indexes():
+		buttons[index].icon = icon_textures[randi() % icon_textures.size()]
+	
