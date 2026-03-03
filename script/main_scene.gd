4ae58d4dc40a9d8e63aac9eb0cfbd135bae15f07
Kathleen Povadora <60345682+kaeri-gg@users.noreply.github.com>
2026-03-03 14:21:17 +0200

    Add main scene, start button, assets, themes
    Add game UI and assets: import background and 8 sticker images with Godot import settings, add theme resources for main scene, grid, timer and counter. Create main_scene.tscn (UI layout: grid, buttons, timer, score, counter) and game_menu updates to use a StartButton with an attached script and pressed signal. Add scripts: start_button.gd (changes to main_scene) and skeleton main_scene.gd to wire up buttons and scene logic.

---
+++ script/main_scene.gd
@@ -0,0 +1,28 @@
+class_name MainScene
+extends Control
+
+@onready var button: Button = %Button
+@onready var button_2: Button = %Button2
+@onready var button_3: Button = %Button3
+@onready var button_4: Button = %Button4
+@onready var button_5: Button = %Button5
+@onready var button_6: Button = %Button6
+@onready var button_7: Button = %Button7
+@onready var button_8: Button = %Button8
+@onready var button_9: Button = %Button9
+@onready var button_10: Button = %Button10
+@onready var button_11: Button = %Button11
+@onready var button_12: Button = %Button12
+@onready var button_13: Button = %Button13
+@onready var button_14: Button = %Button14
+@onready var button_15: Button = %Button15
+@onready var button_16: Button = %Button16
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
