11e9e603120e320aea9c193b5ca2ed69e269066c
Kathleen Povadora <60345682+kaeri-gg@users.noreply.github.com>
2026-03-04 15:50:56 +0200

    Use GridContainer for tile layout
    Replace the previous nested HBox/VBox containers with a single GridContainer (columns=4) and move all Button nodes under it. Simplify the script by removing per-button onready vars and instead grabbing children from grid_container, dynamically connecting their pressed signals, and managing correct tile indexes via get_icon_indexes() and a correct_tiles dictionary. Also add a basic on_button_pressed handler and tidy naming. Update theme grid_styles.tres to use a warm background color for the button styleboxes.

--- script/main_scene.gd
+++ script/main_scene.gd
@@ -1,32 +1,12 @@
 class_name MainScene
 extends Control
 
-@onready var button_1: Button = %Button
-@onready var button_2: Button = %Button2
-@onready var button_3: Button = %Button3
-@onready var button_4: Button = %Button4
-@onready var button_5: Button = %Button5
-@onready var button_6: Button = %Button6
-@onready var button_7: Button = %Button7
-@onready var button_8: Button = %Button8
-@onready var button_9: Button = %Button9
-@onready var button_10: Button = %Button10
-@onready var button_11: Button = %Button11
-@onready var button_12: Button = %Button12
-@onready var button_13: Button = %Button13
-@onready var button_14: Button = %Button14
-@onready var button_15: Button = %Button15
-@onready var button_16: Button = %Button16
+@onready var grid_container: GridContainer = %GridContainer
 
-@onready var grid_container_1_4: HBoxContainer = %"GridContainer1-4"
-@onready var grid_container_5_8: HBoxContainer = %"GridContainer5-8"
-@onready var grid_container_9_12: HBoxContainer = %"GridContainer9-12"
-@onready var grid_container_13_16: HBoxContainer = %"GridContainer13-16"
-
-
-var buttons: Array[Button]
+var buttons: Array[Node]
 var chocolate_count: int
 var streak_count: int
+var correct_tiles: Dictionary[int, bool] = {}
 
 var icon_textures = [
 	preload("res://assets/stickers/bueno.png"),
@@ -47,41 +27,33 @@ var dummy_textures = [
 func reset() -> void:
 	chocolate_count = 4
 	streak_count = 0
-	
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	reset()
-	buttons = [
-		button_1,
-		button_2,
-		button_3,
-		button_4,
-		button_5,
-		button_6,
-		button_7,
-		button_8,
-		button_9,
-		button_10,
-		button_11,
-		button_12,
-		button_13,
-		button_14,
-		button_15,
-		button_16
-	]
-		
+	buttons = grid_container.get_children()
+	
+	for i in buttons.size():
+		buttons[i].pressed.connect(on_button_pressed.bind(i))
+
 	assign_icons()
 
-func get_indexes() -> Dictionary[int, bool]:
-	var indexes: Dictionary[int, bool] = {}
+func get_icon_indexes() -> Dictionary[int, bool]:
+	var indexes_of_icons: Dictionary[int, bool] = {}
 	
-	while indexes.size() < chocolate_count:
+	while indexes_of_icons.size() < chocolate_count:
 		var random_index = randi_range(0, buttons.size() -1)
-		indexes.set(random_index, true)
+		indexes_of_icons.set(random_index, true)
 		
-	return indexes
+	return indexes_of_icons
 
 func assign_icons() -> void:
-	for index in get_indexes():
+	
+	correct_tiles = get_icon_indexes()
+	
+	for index in correct_tiles:
 		buttons[index].icon = icon_textures[randi() % icon_textures.size()]
+		
+func on_button_pressed(index: int) -> void:
+	print("clicked ", index)
 	
